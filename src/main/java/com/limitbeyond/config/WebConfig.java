5bd0595f95015353f55ef4153bc6cfc65030f144
Bibek <zhabibek4u@gmail.com>
2025-08-15 18:18:41 +0530

    dg

--- src/main/java/com/limitbeyond/config/WebConfig.java
+++ src/main/java/com/limitbeyond/config/WebConfig.java
@@ -7,22 +7,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                                .allowedOrigins(
-                                                "http://localhost:8081",
-                                                "http://localhost:3000",
-                                                "http://localhost:5173")
-                                .allowedMethods(
-                                                "GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
-                                .allowedHeaders("*")
-
-                                .exposedHeaders(
-                                                "Authorization",
-                                                "Access-Control-Allow-Origin",
-                                                "Access-Control-Allow-Credentials")
-                                .allowCredentials(true)
-                                .maxAge(3600);
-        }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") // Allows any origin
+                .allowedMethods(
+                        "GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
+                .allowedHeaders("*")
+                .exposedHeaders(
+                        "Authorization",
+                        "Access-Control-Allow-Origin",
+                        "Access-Control-Allow-Credentials")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
 }
